6974a92a5bcf24be6815fdc79f0ad7b875a21753
Rangasai <rangasaimangalagiri@gmail.com>
2025-04-09 22:31:36 +0530

    C code tested

--- solutions/solution.c
+++ solutions/solution.c
@@ -2,7 +2,31 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void dfs(char **grid, int gridSize, int *gridColSize, int i, int j)
+{
+    if (i < 0 || i >= gridSize || j < 0 || j >= *gridColSize || grid[i][j] == '0')
+    {
+        return;
+    }
+    grid[i][j] = '0'; // Mark the cell as visited
+    dfs(grid, gridSize, gridColSize, i + 1, j);
+    dfs(grid, gridSize, gridColSize, i - 1, j);
+    dfs(grid, gridSize, gridColSize, i, j + 1);
+    dfs(grid, gridSize, gridColSize, i, j - 1);
+}
 int numIslands(char **grid, int gridSize, int *gridColSize)
 {
-    // write your code
+    int count = 0;
+    for (int i = 0; i < gridSize; i++)
+    {
+        for (int j = 0; j < *gridColSize; j++)
+        {
+            if (grid[i][j] == '1')
+            {
+                count += 1;
+                dfs(grid, gridSize, gridColSize, i, j);
+            }
+        }
+    }
+    return count;
 }
