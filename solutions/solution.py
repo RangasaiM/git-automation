43a7fd6b8fb7e773b9c91295cedffa4f2985a3fc
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2025-04-30 14:15:39 +0530

    Update solution.py

--- solutions/solution.py
+++ solutions/solution.py
@@ -9,21 +9,6 @@ sys.setrecursionlimit(10000)
 from typing import List
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(grid, i, j):
-            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
-                return 
-            grid[i][j] = '#'
-            dfs(grid, i+1, j)
-            dfs(grid, i-1, j)
-            dfs(grid, i, j+1)
-            dfs(grid, i, j-1)
-        count = 0
-        if not grid:
-            return 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    dfs(grid, i, j)
-                    count += 1
-                    
-        return count
+        # write your code here
+       pass
+        
