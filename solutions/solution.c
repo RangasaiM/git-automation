78d246def334ddbf040fbd8c472a03601fddfdfa
RangasaiM <123390340+RangasaiM@users.noreply.github.com>
2025-04-30 13:00:16 +0530

    Update solution.c

--- solutions/solution.c
+++ solutions/solution.c
@@ -5,5 +5,5 @@
 
 int numIslands(char** grid, int gridSize, int* gridColSize){
    //Write your code here
-    return 0; // remove this line while submiting the code
+    return 0; // remove this line while submiting the code.c
 }
